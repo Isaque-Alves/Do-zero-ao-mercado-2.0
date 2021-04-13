0cc6eaf7785c42db99bfad30ac25fe164a333bb4
Isaque <isaquea785@gmail.com>
2021-04-13 14:08:27 -0300

    seção 3 - aula 26

---
+++ exercicios-web/fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {};
+prod1.nome = "Smart TV 2k21";
+prod1.preco = 4998.9;
+prod1["Desconto domingo"] = 0.4; //evitar atributos com espaços
+
+console.log(prod1);
+
+const prod2 = {
+  nome: "Camisa Calvin klein",
+  preco: 99.9,
+};
+
+console.log(prod2);
