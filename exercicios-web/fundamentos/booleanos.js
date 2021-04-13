f26a1a594ae76784f77c86c869cf0e4ceff920b4
Isaque <isaquea785@gmail.com>
2021-04-13 13:45:17 -0300

    seção 3 - aula 24

---
+++ exercicios-web/fundamentos/booleanos.js
@@ -0,0 +1,29 @@
+let isAtivo = false;
+isAtivo = true;
+
+isAtivo = 1;
+console.log(!!isAtivo);
+
+console.log("Os verdadeiros... ");
+console.log(!!3);
+console.log(!!-1);
+console.log(!!" ");
+console.log(!!"teste");
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
+
+console.log("Os falsos... ");
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false));
+
+console.log("Finalizando...");
+console.log(!!("" || null || 0 || "epa" || " "));
+
+let nome = "";
+console.log(nome || "Desconhecido");
