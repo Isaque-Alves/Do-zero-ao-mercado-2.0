17850165f04b9dcc16bddc226f72beb708ddf4ac
Isaque <isaquea785@gmail.com>
2021-04-13 11:31:28 -0300

    seção 3 - aula 18

---
+++ exercicios-web/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = "Texto";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1415;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evitar nomes genéricos e siglas.Exemplos:
+let valor = ""; //que valor??
+let numero = 1; //que número??
+let pqp = false; // oque a sigla significa??
