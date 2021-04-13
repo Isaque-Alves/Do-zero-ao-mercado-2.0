77642c96dff3edf3132e2417052f1595ea775f93
Isaque <isaquea785@gmail.com>
2021-04-13 11:25:36 -0300

    seção 3 - aula 17

---
+++ exercicios-web/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,13 @@
+var valor = 3;
+let valor2 = 4;
+
+var valor = 30;
+valor2 = 40;
+console.log(valor, valor2);
+
+valor = 300;
+valor2 = 400;
+console.log(valor, valor2);
+
+const constante = 5;
+console.log(constante);
