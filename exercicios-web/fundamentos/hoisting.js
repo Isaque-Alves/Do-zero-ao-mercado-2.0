a24b2939c17591ae27a34961d8c8de7398ade420
Isaque <isaquea785@gmail.com>
2021-04-14 13:44:35 -0300

    seção 3 - aula 38

---
+++ exercicios-web/fundamentos/hoisting.js
@@ -0,0 +1,11 @@
+//curiosidade
+//Hoisting javascript é um comportamento padrão de mover a declaração para o topo
+
+console.log("a = ", a);
+var a = 2;
+console.log("a = ", a);
+
+//Em let, o hoisting não funciona
+// console.log("b = ", b);
+// let b = 2;
+// console.log("b = ", b);
