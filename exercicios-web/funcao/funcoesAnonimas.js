0b945194fe92394921b72d1de5126bfdc5058e08
Isaque <isaquea785@gmail.com>
2021-04-19 09:54:40 -0300

    seção 5 - aula 76

---
+++ exercicios-web/funcao/funcoesAnonimas.js
@@ -0,0 +1,22 @@
+const soma = function (x, y) {
+  return x + y;
+};
+
+const imrpimirResultado = function (a, b, operacao = soma) {
+  console.log(operacao(a, b));
+};
+
+imrpimirResultado(3, 4);
+imrpimirResultado(3, 4, soma);
+imrpimirResultado(3, 4, function (x, y) {
+  return x - y;
+});
+imrpimirResultado(3, 4, (x, y) => x * y);
+
+const pessoa = {
+  falar: function () {
+    console.log("opa");
+  },
+};
+
+pessoa.falar();
