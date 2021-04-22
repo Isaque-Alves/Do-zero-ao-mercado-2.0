058cb00637324b1f8fc76603237954b174de557d
Isaque <isaquea785@gmail.com>
2021-04-22 12:50:39 -0300

    seção 7 - aula 115

---
+++ exercicios-web/array/map2.js
@@ -0,0 +1,17 @@
+const carrinho = [
+  '{ "nome":"Borracha", "preco": 3.45 }',
+  '{"nome":"Caderno", "preco": 13.90}',
+  '{"nome":"Kit de Lapis", "preco": 41.22}',
+  '{"nome":"Caneta", "preco": 7.50}',
+];
+
+//Retornar um array apenas com os preços
+const precos = carrinho.map((e) => JSON.parse(e).preco);
+console.log(precos);
+
+//outra forma
+const paraObjeto = (json) => JSON.parse(json);
+const apenasPreco = (produto) => produto.preco;
+
+const resultado = carrinho.map(paraObjeto).map(apenasPreco);
+console.log(resultado);
