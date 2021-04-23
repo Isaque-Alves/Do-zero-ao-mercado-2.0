3b63a98653ed98da533819e7dc4d6fa23ec53912
Isaque <isaquea785@gmail.com>
2021-04-23 16:02:42 -0300

    seção 7 - aula 123

---
+++ exercicios-web/array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ["Valentina", "Sther"];
+const filhos = ["Enzo", "Edu"];
+const todos = filhas.concat(filhos, "Fulane");
+
+console.log(todos, filhas, filhos);
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
